a7b2133e868af6b12200e3ca18553b92f4818bbd
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-04-15 23:37:51 -0700

    update:solve routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import DashboardLayout from "./layouts/DashboardLayout";
 import AuthLayout from "./layouts/AuthLayout";
@@ -10,11 +10,12 @@ function App() {
     <Routes>
       <Route
         path="/"
-        element={
-          <div className="text-7xl">
-            Project Template || Please read readme file
-          </div>
-        }
+        // element={
+        //   <div className="text-7xl">
+        //     Project Template || Please read readme file
+        //   </div>
+        // }
+        element={<Navigate to="/auth/login"/>}
       />
 
       <Route path="app" element={<DashboardLayout />}>
